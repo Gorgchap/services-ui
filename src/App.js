6ad479620f7264ee5d2fea8cc68104256aed0efa
Kirill Kazakov <kirill.kazakov@combridge-itc.de>
2019-11-26 22:29:20 +0300

    Template of services-ui (ver 1.0)

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Col, Row } from 'react-bootstrap';
+import React from 'react';
+import Service from "./Service";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <><h2 style={{ textAlign: 'center' }}>Services</h2>
+      <Row bsPrefix={'service-row'}>
+          <Col xs={{span: 6, offset: 3}} sm={{span: 6, offset: 0}} md={4} lg={3}>
+              <Service name={'Service 1'} />
+          </Col>
+          <Col xs={{span: 6, offset: 3}} sm={{span: 6, offset: 0}} md={4} lg={3}>
+              <Service name={'Service 2'} />
+          </Col>
+          <Col xs={{span: 6, offset: 3}} sm={{span: 6, offset: 0}} md={4} lg={3}>
+              <Service name={'Service 3'} />
+          </Col>
+          <Col xs={{span: 6, offset: 3}} sm={{span: 6, offset: 0}} md={4} lg={3}>
+              <Service />
+          </Col>
+      </Row></>
   );
 }
-
 export default App;
---
+++ src/Service.js
@@ -0,0 +1,35 @@
+import './App.css';
+import { Button, Card } from 'react-bootstrap';
+import React, { useState, useEffect } from "react";
+
+const notStarted = <button type="button" className="btn btn-secondary btn-circle btn-xl">Not started</button>;
+const progress = <button type="button" className="btn btn-danger btn-circle">In progress</button>;
+const ready = <button type="button" className="btn btn-success btn-circle">Ready</button>;
+
+function Service(props) {
+    const [isReady, setIsReady] = useState(false);
+    const [isNotStarted, setIsNotStarted] = useState(true);
+    useEffect(() => {
+        if (!isNotStarted) {
+            const timer = setTimeout(() => setIsReady(!isReady), 5000);
+            return () => clearTimeout(timer);
+        }
+    });
+    function onClick() {
+        if (isNotStarted) { setIsNotStarted(false); }
+        else { setIsReady(false); setIsNotStarted(true); }
+    }
+    return <Card className="service-card" {...props}>
+        <Card.Body style={{ padding: '.75rem' }}>
+            <Card.Title style={{ textAlign: 'center' }}>{props.name ? props.name : 'Default service'}</Card.Title>
+            {isNotStarted ? notStarted : isReady ? ready : progress}
+            <Card.Text style={{ margin: 0 }}>
+                Some quick example text to build on the card title and make up the bulk of the card's content.
+            </Card.Text>
+            <Button onClick={onClick} style={{ display: 'block', margin: '0 auto', width: '25%' }} variant="primary">
+                {isNotStarted ? 'Start' : 'Stop'}
+            </Button>
+        </Card.Body>
+    </Card>
+}
+export default Service;
